804929d46f8755dd46d4106cd9d322d6050a14aa
ChloeZeng <chloezzz@Chloes-MacBook.local>
2025-09-20 17:22:40 -0700

    created partners.js page and display on other pages

---
+++ partners.js
@@ -0,0 +1,32 @@
+// partners.js
+document.addEventListener("DOMContentLoaded", function () {
+    var fileNames = [
+    "partner-bustour.png",
+    "partner-cabinrental.png",
+    "partner-campingadv.png",
+    "partner-collegetours.png",
+    "partner-rentalbike.png",
+    "partner-tourgroup.png"
+  ];
+  
+  var captions = [
+    "Bus Tour",
+    "Cabin Rental",
+    "Camping Adventure",
+    "College Tours",
+    "Rental Bike",
+    "Tour Group"
+  ];
+
+  var imageList = []; 
+
+
+  for (var i = 0; i < fileNames.length; i++) {
+  var image = "<li class='partner'>" +
+              "<img src='images/partners/" + fileNames[i] + "'>" +
+              "</li>";
+  imageList.push(image);
+}
+
+  document.getElementById("partners").innerHTML = imageList.join("");
+});
