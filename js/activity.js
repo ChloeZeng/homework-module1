5af3746132b37410986e6cccede7604ddd89fb31
ChloeZeng <chloezzz@Chloes-MacBook.local>
2025-10-04 22:51:30 -0700

    add displaySelected info and css effect

--- js/activity.js
+++ js/activity.js
@@ -1,21 +1,29 @@
-$(document).ready(function(){
+// User interaction with table cells
+$(document).ready(function() {
+  $("td").click(function () { // when user clicks a table data cell
+    var content = $(this).text(); // get cell text
+    var colIndex = $(this).index(); // get column index of clicked cell
+    var cliffName = $("th").eq(colIndex).text(); // get corresponding Cliff name from header row
 
-})
-
-$("td").click(function(){
-    //if "not available"
-    if ($(this).text().trim() === "Not Available" || $(this).is("th")) {
-        return;
+    // Skip "Not Available" and first column (Activity titles)
+    if (content === "Not Available" || colIndex === 0) {
+      return;
     }
 
-    $(this).toggleClass("selected");
+    // Toggle highlight
+    $(this).toggleClass("tdhighlight");
 
-    //display hand
-    $("td").hover(function(){
-    if ($(this).text().trim() !== "Not Available" && !$(this).is("th")) {
-      $(this).css("cursor", "pointer");
+    // Add or remove from result box
+    if ($(this).hasClass("tdhighlight")) {
+      $("#displaySelected").css("visibility", "visible").css("margin-top", "2em");
+      $("#result").append("<p>" + content + " <span class='cliffName'>at " + cliffName + "</span></p>");
     } else {
-      $(this).css("cursor", "default");
+      $("#result p:contains('" + content + "')").remove();
+      // If no activities selected, hide the display box
+      if ($("#result").children().length === 0) {
+        $("#displaySelected").css("visibility", "hidden").css("margin-top", "0");
+      }
     }
   });
 });
+
