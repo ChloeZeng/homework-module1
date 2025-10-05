a9e92f9858ab851cbf405dc9bb7c08090ef0520b
ChloeZeng <chloezzz@Chloes-MacBook.local>
2025-10-04 17:44:20 -0700

    move activity.js to js folder

--- activity.js
+++ activity.js
@@ -1,3 +1,12 @@
 $(document).ready(function(){
-    
+
 })
+
+$("td").click(function(){
+    //if "not available"
+    if ($(this).text().trim() === "Not Available" || $(this).is("th")) {
+        return;
+    }
+
+    $(this).toggleClass("selected");
+});
