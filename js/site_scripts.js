ed6c177764b576615adf9bdf659b0ee5df694621
ChloeZeng <yitian_815@hotmail.com>
2025-10-17 21:25:25 -0700

    fix partner section

--- js/site_scripts.js
+++ js/site_scripts.js
@@ -1,33 +1,37 @@
-// partners.js
+// partners.js — Bootstrap-compatible
 document.addEventListener("DOMContentLoaded", function () {
-    var fileNames = [
+  const fileNames = [
     "partner-bustour.png",
     "partner-cabinrental.png",
     "partner-campingadv.png",
     "partner-collegetours.png",
     "partner-rentalbike.png",
-    "partner-tourgroup.png"
+    "partner-tourgroup.png",
   ];
-  
-  var captions = [
+
+  const captions = [
     "Bus Tour",
     "Cabin Rental",
     "Camping Adventure",
     "College Tours",
     "Rental Bike",
-    "Tour Group"
+    "Tour Group",
   ];
 
-  var imageList = []; 
+  const partnersContainer = document.getElementById("partners");
+  let html = "";
 
+  // Use Bootstrap grid columns
+  for (let i = 0; i < fileNames.length; i++) {
+    html += `
+      <div class="col-6 col-sm-6 col-md-4 col-lg-2 mb-4 text-center">
+        <div class="partner">
+          <img src="images/partners/${fileNames[i]}" alt="${captions[i]}" class="partner-img img-fluid">
+        </div>
+        <p class="partner-caption">${captions[i]}</p>
+      </div>
+    `;
+  }
 
-  for (var i = 0; i < fileNames.length; i++) {
-  var image = "<li class='partner'>" +
-              "<img src='images/partners/" + fileNames[i] + "'>" +
-              "</li>";
-  imageList.push(image);
-}
-
-  document.getElementById("partners").innerHTML = imageList.join("");
+  partnersContainer.innerHTML = html;
 });
-
